06a1a20ff589958e56b27e757465ddb78705b4b4
Pedro Valravn <pedrovalravn@outlook.com>
2023-10-21 15:43:04 -0300

    initial commit

---
+++ src/main/java/me/valravn/javaapirestful/JavaApiRestfulApplication.java
@@ -0,0 +1,13 @@
+package me.valravn.javaapirestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApiRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApiRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/valravn/javaapirestful/Application.java
@@ -0,0 +1,13 @@
+package me.valravn.javaapirestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Application {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
